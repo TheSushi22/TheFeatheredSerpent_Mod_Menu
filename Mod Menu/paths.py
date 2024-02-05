dc6e46e0cbbf2fca4889b3a3f3c7d6ca6ec3297c
The_Sushi <101234230+TheSushi22@users.noreply.github.com>
2024-02-05 15:44:53 -0600

    Update paths.py

--- Mod Menu/paths.py
+++ Mod Menu/paths.py
@@ -5,22 +5,23 @@ from pymem.ptypes import RemotePointer
 #Game Name
 pm = Pymem("TheFeatheredSerpent_KS_V1.7.exe")
 
-#Offsets
+#Game Offset
 
+game_offset = pm.base_address + 0x023D2BC0
+
+#Offsets
 blood_gems_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xA28]
-blood_gems_address = pm.base_address + 0x023D2BC0
 
 coins_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xA58]
-coins_address = pm.base_address + 0x023D2BC0
 
 health_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0x230]
-health_address = pm.base_address + 0x023D2BC0
 
 arrow_speed_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xBD8]
-arrow_speed_address = pm.base_address + 0x023D2BC0
+
+player_speed_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xB78]
+
 
 #Where The Magic Happens
-
 def getPointerAddress(base, offsets):
     remote_pointer = RemotePointer(pm.process_handle, base)
     for offset in offsets:
@@ -28,13 +29,15 @@ def getPointerAddress(base, offsets):
             remote_pointer = RemotePointer(pm.process_handle, remote_pointer.value + offset)
         else:
             return remote_pointer.value + offset
-        
+
+
 #Putting It Together
+blood_gems = getPointerAddress(game_offset, blood_gems_offsets)
 
-blood_gems = getPointerAddress(blood_gems_address, blood_gems_offsets)
+coins = getPointerAddress(game_offset, coins_offsets)
 
-coins = getPointerAddress(coins_address, coins_offsets)
+health = getPointerAddress(game_offset, health_offsets)
 
-health = getPointerAddress(health_address, health_offsets)
+arrow_speed = getPointerAddress(game_offset, arrow_speed_offsets)
 
-arrow_speed = getPointerAddress(arrow_speed_address, arrow_speed_offsets)
+player_speed = getPointerAddress(game_offset, player_speed_offsets)
