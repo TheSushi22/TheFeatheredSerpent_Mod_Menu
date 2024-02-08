1603c7242e6b44e322af21d18c1dc1d60e5899e9
The_Sushi <101234230+TheSushi22@users.noreply.github.com>
2024-02-07 19:47:49 -0600

    Update paths.py

--- Mod Menu/paths.py
+++ Mod Menu/paths.py
@@ -18,7 +18,14 @@ health_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0x230]
 
 arrow_speed_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xBD8]
 
+damage_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xAD0]
+
+ammo_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xA88]
+
+machine_gun_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xBA8]
+
 player_speed_offsets = offsets=[0x1E0, 0x58, 0x20, 0x70, 0x58, 0x20, 0xB78]
+
 
 
 #Where The Magic Happens
@@ -39,5 +46,11 @@ coins = getPointerAddress(game_offset, coins_offsets)
 health = getPointerAddress(game_offset, health_offsets)
 
 arrow_speed = getPointerAddress(game_offset, arrow_speed_offsets)
+
+damage = getPointerAddress(game_offset, damage_offsets)
+
+ammo = getPointerAddress(game_offset, ammo_offsets)
+
+machine_gun = getPointerAddress(game_offset, machine_gun_offsets)
 
 player_speed = getPointerAddress(game_offset, player_speed_offsets)
